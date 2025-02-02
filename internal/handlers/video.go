d17373893db897f94bcff11c0b49e33189bd77ec
PaoloTorregroza <paolinskypdtn@gmail.com>
2025-02-02 13:56:27 -0500

    Remove unused serveSegment fhandler

--- internal/handlers/video.go
+++ internal/handlers/video.go
@@ -44,27 +44,3 @@ func ServeVideo(c *gin.Context) {
 func fileIsMPD(fileName string) bool {
 	return strings.HasSuffix(fileName, ".mpd")
 }
-
-func ServeVideoSegment(c *gin.Context) {
-	videoId := c.Param("videoId")
-	segmentId := c.Param("segmentId")
-	segmentFile := "video/" + videoId + segmentId
-
-	repo := c.MustGet(utils.S3_REPO_CTX_KEY).(*repositorioes.S3Repo)
-
-	fileResp, err := repo.GetObjectByFileName(segmentFile)
-	if err != nil {
-		log.Printf("Error getting video segment from S3: %v", err)
-		c.String(http.StatusInternalServerError, "Error getting video segment from S3")
-		return
-	}
-	defer fileResp.Body.Close()
-
-	c.DataFromReader(
-		http.StatusOK,
-		*fileResp.ContentLength,
-		"application/dash+xml",
-		fileResp.Body,
-		map[string]string{},
-	)
-}
